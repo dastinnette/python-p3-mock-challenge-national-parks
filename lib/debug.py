0a651d909773fe487f48d63cd927bbc38d7dad21
David Stinnette <dastinnette@gmail.com>
2023-10-31 08:23:21 -0400

    Completed national parks lab

--- lib/debug.py
+++ lib/debug.py
@@ -8,4 +8,12 @@ from classes.many_to_many import Trip
 if __name__ == '__main__':
     print("HELLO! :) let's debug :vibing_potato:")
 
+    v1 = Visitor("Emiley")
+    v2 = Visitor("Adam")
+    n1 = NationalPark("Zion")
+    n2 = NationalPark("Arches")
+    t1 = Trip(v1, n1, "September 1st", "October 1st")
+    t1 = Trip(v1, n2, "April 1st", "May 1st")
+    t1 = Trip(v2, n1, "January 1st", "February 1st")
+
     ipdb.set_trace()
